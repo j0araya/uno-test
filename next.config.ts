c9434d59e09c512af996e73b1eae50d3ae2e93f0
Jonathan <jonathan.araya.m@gmail.com>
2024-10-30 04:02:49 -0300

    chore(path)

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/uno-test",
-  output: "export",
   images: {
     unoptimized: true,
     remotePatterns: [
